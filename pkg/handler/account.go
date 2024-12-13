d713759bf366c776db92bb467d21026d47192f2d
Aurivena <vladkonovalov20060@gmail.com>
2024-12-13 11:54:12 +0500

    master - добавляет документацию

--- pkg/handler/account.go
+++ pkg/handler/account.go
@@ -6,8 +6,16 @@ import (
 	"vertexUP/pkg/usecase"
 )
 
+// @Summary      Обновить данные пользователя
+// @Description  Обновляет данные пользователя, которые будут отправлены
+// @Tags         Аккаунт
+// @Accept       json
+// @Produce      json
+// @Param        models.UpdateInfoAccountInput body models.UpdateInfoAccountInput true "Входные данные"
+// @Success      204 {object} string "NoContent"
+// @Failure      400 {object} string "BadRequest"
+// @Router       /account [put]
 func (h Handler) updateInfoAccount(c *gin.Context) {
-
 	token := c.GetHeader("Authorization")
 	if token == "" {
 		h.sendResponseSuccess(c, nil, usecase.BadRequest)
